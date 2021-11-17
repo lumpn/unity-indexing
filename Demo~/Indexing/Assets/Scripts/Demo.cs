b578b5b28dfa02cc2f1d97044de8c3d39131f08b
lumpn <lumpn@users.noreply.github.com>
2021-11-17 22:06:09 +0800

    Added profiling

--- Demo~/Indexing/Assets/Scripts/Demo.cs
+++ Demo~/Indexing/Assets/Scripts/Demo.cs
@@ -2,8 +2,8 @@
 // MIT License
 // Copyright(c) 2021 Jonas Boetel
 //----------------------------------------
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Profiling;
 
@@ -15,73 +15,15 @@ namespace Lumpn.Collections.Indexing.Demo
 
         void Start()
         {
-            var list = Enumerable.Range(0, numItems).ToList();
-            var items = list.ToArray();
+            var items = Enumerable.Range(0, numItems);
+            var list = items.ToList();
+            var array = items.ToArray();
+            var set = new HashSet<int>(items);
 
-            {
-                Profiler.BeginSample("ValueTuple JIT");
-                var tuple = System.ValueTuple.Create(0, 0);
-                Profiler.EndSample();
-
-                Profiler.BeginSample("ValueTuple deconstruct");
-                var (a, b) = tuple;
-                Profiler.EndSample();
-                Debug.Assert(a == b);
-
-                var listEnumerator = list.GetEnumerator();
-                Profiler.BeginSample("Enumerator JIT");
-                var enumerator = new Enumerator<int>(listEnumerator);
-                Profiler.EndSample();
-                Debug.Assert(enumerator.MoveNext());
-
-                Profiler.BeginSample("Indexer JIT");
-                var indexer = new Indexer<int>(items);
-                Profiler.EndSample();
-                Debug.Assert(indexer.GetEnumerator().MoveNext());
-            }
-
-            Profiler.BeginSample("for loop (array)");
-            var sum = 0;
-            for (int i = 0; i < items.Length; i++)
-            {
-                sum += items[i];
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("foreach loop (array)");
-            foreach (var item in items)
-            {
-                sum += item;
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("indexed foreach loop (array)");
-            foreach (var tuple in items.Indexed())
-            {
-                sum += tuple.Item2;
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("indexed foreach loop second time (array)");
-            foreach (var tuple in items.Indexed())
-            {
-                sum += tuple.Item2;
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("deconstructed foreach loop (array)");
-            foreach (var (index, item) in items.Indexed())
-            {
-                sum += item;
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("deconstructed foreach loop over index (array)");
-            foreach (var (index, item) in items.Indexed())
-            {
-                sum += index;
-            }
-            Profiler.EndSample();
+            RunLoops(items, "Enumerable");
+            RunLoops(array, "Array");
+            RunLoops(list, "List");
+            RunLoops(set, "HashSet");
         }
 
         void Update()
@@ -89,9 +31,56 @@ namespace Lumpn.Collections.Indexing.Demo
             Debug.Break();
         }
 
-        private void RunLoops(IEnumerable<int> items, string typeName)
+        private int RunLoops(IEnumerable<int> items, string typeName)
         {
+            var sum = 0;
 
+            Profiler.BeginSample($"{typeName} JIT");
+            foreach (var (index, item) in items.Indexed())
+            {
+                sum += index;
+                sum += item;
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample($"{typeName} foreach");
+            foreach (var item in items)
+            {
+                sum += item;
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample($"{typeName} foreach Indexed");
+            foreach (var (index, item) in items.Indexed())
+            {
+                sum += item;
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample($"{typeName} foreach Select");
+            foreach (var (index, item) in items.Select(System.ValueTuple.Create<int, int>))
+            {
+                sum += item;
+            }
+            Profiler.EndSample();
+
+            Profiler.BeginSample($"{typeName} foreach Wrap");
+            foreach (var (index, item) in Wrap(items))
+            {
+                sum += item;
+            }
+            Profiler.EndSample();
+
+            return sum;
+        }
+
+        private static IEnumerable<System.ValueTuple<int, T>> Wrap<T>(IEnumerable<T> source)
+        {
+            int index = 0;
+            foreach (var item in source)
+            {
+                yield return System.ValueTuple.Create(index++, item);
+            }
         }
     }
 }
