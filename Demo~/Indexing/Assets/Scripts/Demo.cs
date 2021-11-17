b6e167fb8407015acc223c40fcdc87b31ba43ec1
lumpn <lumpn@users.noreply.github.com>
2021-11-18 00:10:32 +0800

    KeyValuePair tests

--- Demo~/Indexing/Assets/Scripts/Demo.cs
+++ Demo~/Indexing/Assets/Scripts/Demo.cs
@@ -21,6 +21,13 @@ namespace Lumpn.Collections.Indexing.Demo
             var array = items.ToArray();
             var set = new HashSet<int>(items);
 
+            //Profiler.BeginSample("JIT");
+            //RunLoops(items, "Enumerable");
+            //RunLoops(array, "Array");
+            //RunLoops(list, "List");
+            //RunLoops(set, "HashSet");
+            //Profiler.EndSample();
+
             for (int i = 0; i < 10; i++)
             {
                 RunLoops(items, "Enumerable");
@@ -28,23 +35,6 @@ namespace Lumpn.Collections.Indexing.Demo
                 RunLoops(list, "List");
                 RunLoops(set, "HashSet");
             }
-
-            int sum = 0;
-            Profiler.BeginSample("Array foreach optimized");
-            foreach (var item in array)
-            {
-                sum += item;
-            }
-            Profiler.EndSample();
-
-            Profiler.BeginSample("Array for optimized");
-            int count = array.Length;
-            for (int i = 0; i < count; i++)
-            {
-                sum += array[i];
-            }
-            Profiler.EndSample();
-            Debug.Log(sum);
         }
 
         void Update()
@@ -55,15 +45,6 @@ namespace Lumpn.Collections.Indexing.Demo
         private int RunLoops(IEnumerable<int> items, string typeName)
         {
             int sum = 0;
-
-            Profiler.BeginSample($"{typeName} JIT");
-            sum += RunForeach(items);
-            sum += RunIndexed(items);
-            sum += RunWrap(items);
-            sum += RunSelectAnonymous(items);
-            sum += RunSelectTuple(items);
-            sum += RunSelectValueTuple(items);
-            Profiler.EndSample();
 
             Profiler.BeginSample($"{typeName} foreach");
             sum += RunForeach(items);
@@ -84,9 +65,22 @@ namespace Lumpn.Collections.Indexing.Demo
             Profiler.BeginSample($"{typeName} foreach Select Tuple");
             sum += RunSelectTuple(items);
             Profiler.EndSample();
+            Profiler.BeginSample($"{typeName} foreach Select Tuple2");
+            sum += RunSelectTuple2(items);
+            Profiler.EndSample();
 
             Profiler.BeginSample($"{typeName} foreach Select ValueTuple");
             sum += RunSelectValueTuple(items);
+            Profiler.EndSample();
+            Profiler.BeginSample($"{typeName} foreach Select ValueTuple2");
+            sum += RunSelectValueTuple2(items);
+            Profiler.EndSample();
+
+            Profiler.BeginSample($"{typeName} foreach Select KeyValuePair");
+            sum += RunSelectKeyValuePair(items);
+            Profiler.EndSample();
+            Profiler.BeginSample($"{typeName} foreach Select KeyValuePair2");
+            sum += RunSelectKeyValuePair2(items);
             Profiler.EndSample();
 
             return sum;
@@ -115,8 +109,20 @@ namespace Lumpn.Collections.Indexing.Demo
         private static int RunSelectTuple(IEnumerable<int> items)
         {
             int sum = 0;
+            foreach (var (index, item) in items.Select((a, b) => new System.Tuple<int, int>(a, b)))
+            {
+                sum += index;
+                sum += item;
+            }
+            return sum;
+        }
+
+        private static int RunSelectTuple2(IEnumerable<int> items)
+        {
+            int sum = 0;
             foreach (var (index, item) in items.Select(System.Tuple.Create<int, int>))
             {
+                sum += index;
                 sum += item;
             }
             return sum;
@@ -125,11 +131,50 @@ namespace Lumpn.Collections.Indexing.Demo
         private static int RunSelectValueTuple(IEnumerable<int> items)
         {
             int sum = 0;
-            foreach (var (index, item) in items.Select(System.ValueTuple.Create<int, int>))
+            foreach (var (index, item) in items.Select((a, b) => new System.ValueTuple<int, int>(a, b)))
             {
+                sum += index;
                 sum += item;
             }
             return sum;
+        }
+
+        private static int RunSelectValueTuple2(IEnumerable<int> items)
+        {
+            int sum = 0;
+            foreach (var (index, item) in items.Select(System.ValueTuple.Create<int, int>))
+            {
+                sum += index;
+                sum += item;
+            }
+            return sum;
+        }
+
+        private static int RunSelectKeyValuePair(IEnumerable<int> items)
+        {
+            int sum = 0;
+            foreach (var kvp in items.Select((index, item) => new KeyValuePair<int, int>(index, item)))
+            {
+                sum += kvp.Key;
+                sum += kvp.Value;
+            }
+            return sum;
+        }
+
+        private static int RunSelectKeyValuePair2(IEnumerable<int> items)
+        {
+            int sum = 0;
+            foreach (var kvp in items.Select(CreatePair))
+            {
+                sum += kvp.Key;
+                sum += kvp.Value;
+            }
+            return sum;
+        }
+
+        private static KeyValuePair<int, int> CreatePair(int a, int b)
+        {
+            return new KeyValuePair<int, int>(a, b);
         }
 
         private static int RunIndexed(IEnumerable<int> items)
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Indexing
-Indexed foreach loops
+Indexed `foreach` loops with minimal overhead.
 
 # Performance
 
@@ -11,8 +11,8 @@ Indexed foreach loops
 | List | Indexed | 40 | 3.44 |
 | List | Wrap coroutine | 104 | 3.47 |
 | List | Select ValueTuple | 232 | 4.88 |
-| List | Select Tuple | 2.3 M | 16.48 |
-| List | Select anonymous | 2.3 M | 16.99 |
+| List | Select Tuple | 2.3 M | 17.07 |
+| List | Select anonymous | 2.3 M | 16.06 |
 
 | Collection | Method | GC Alloc B | Time ms |
 |------------|--------|----------|---------|
@@ -22,5 +22,20 @@ Indexed foreach loops
 | Array | Indexed | 32 | 3.99 |
 | Array | Wrap coroutine | 96 | 4.42 |
 | Array | Select ValueTuple | 224 | 5.87 |
-| Array | Select Tuple | 2.3 M | 18.42 |
-| Array | Select anonymous | 2.3 M | 17.28 |
+| Array | Select Tuple | 2.3 M | 18.14 |
+| Array | Select anonymous | 2.3 M | 16.85 |
+
+| Collection | Method | GC Alloc B | Time ms |
+|------------|--------|----------|---------|
+| Enumerable.Range | `foreach` | 36 | 1.34 |
+| Enumerable.Range | Indexed | 36 | 3.36 |
+| Enumerable.Range | Wrap coroutine | 100 | 3.44 |
+| Enumerable.Range | Select ValueTuple | 228 | 4.86 |
+| Enumerable.Range | Select Tuple | 2.3 M | 17.04 |
+| Enumerable.Range | Select anonymous | 2.3 M | 15.99 |
+
+# Notes
+
+- Do not use anonymous types.
+- Use `ValueTuple` instead of `Tuple`.
+- `KeyValuePair` is also okay but does not provide deconstruction.
